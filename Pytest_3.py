055a4b2c1b30dd8fa8c182dd16df5938c8f47863
amar13617 <amar13617@gmail.com>
2023-01-12 21:32:33 +0530

    average

--- Pytest_3.py
+++ Pytest_3.py
@@ -14,8 +14,16 @@ if num % 5 == 0:
 else:
     print("not multiple of 5")
 
+#Python program to check whether the given integer is a multiple of both 5 and 7
 num = 42
 if ((num % 5 == 0) and (num % 7 == 0)):
     print("yes")
 else:
     print("no")
+
+#Python program to find the average of 10 numbers using for loop
+sm = 0
+for i in range(1,11):
+    sm = sm + i
+    result = sm /10
+print(result)
