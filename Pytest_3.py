0be57023d53703146bd761a71a9507ba867cdf4c
amar13617 <amar13617@gmail.com>
2023-01-12 21:15:53 +0530

    check multiple of 5 or not

--- Pytest_3.py
+++ Pytest_3.py
@@ -5,4 +5,11 @@ for i in a_list:
     sm = sm+i
     result = sm/len(a_list)
     
-print(result)
+#print(result)
+
+#Python program to check whether the given integer is a multiple of 5
+num = 25
+if num % 5 == 0:
+    print("multiple of 5")
+else:
+    print("not multiple of 5")
